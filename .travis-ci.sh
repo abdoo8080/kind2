5dbe0830a15430c4f63b0ddbf3e11e607314fa0f
Abdalrhman M Mohamed <abdoo8080@outlook.com>
2020-06-12 00:18:39 -0500

    Fix errors with travis script.
    Signed-off-by: Abdalrhman M Mohamed <abdoo8080@outlook.com>

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -2,7 +2,9 @@
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
   z3_version="z3-4.7.1-x64-ubuntu-14.04"
   install_dir="/usr/bin/z3"
-  sudo apt install libzmq3-dev
+  echo "deb http://download.opensuse.org/repositories/network:/messaging:/zeromq:/release-stable/Debian_9.0/ ./" >> /etc/apt/sources.list
+  wget https://download.opensuse.org/repositories/network:/messaging:/zeromq:/release-stable/Debian_9.0/Release.key -O- | sudo apt-key add
+  apt-get install libzmq3-dev
 elif [ "$TRAVIS_OS_NAME" = "osx" ]; then 
   z3_version="z3-4.7.1-x64-osx-10.11.6"
   install_dir="/usr/local/bin/z3"
@@ -15,6 +17,7 @@ sudo cp "${z3_version}/bin/z3" $install_dir
 
 # Retrieve opam.
 wget -qq https://raw.githubusercontent.com/ocaml/opam/master/shell/install.sh -O - | sh -s
+opam init
 eval $(opam config env)
 
 # Install ocaml packages needed for Kind 2.
