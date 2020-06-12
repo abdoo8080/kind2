14c13f2c058a1bbe1f0cfa9f5587807e50b7bc71
Abdalrhman M Mohamed <abdoo8080@outlook.com>
2020-06-11 23:57:57 -0500

    Use package managers to install ZMQ.
    Signed-off-by: Abdalrhman M Mohamed <abdoo8080@outlook.com>

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -2,30 +2,19 @@
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
   z3_version="z3-4.7.1-x64-ubuntu-14.04"
   install_dir="/usr/bin/z3"
+  sudo apt install libzmq3-dev
 elif [ "$TRAVIS_OS_NAME" = "osx" ]; then 
   z3_version="z3-4.7.1-x64-osx-10.11.6"
   install_dir="/usr/local/bin/z3"
+  brew install zeromq
 fi
 
 wget "https://github.com/Z3Prover/z3/releases/download/z3-4.7.1/${z3_version}.zip"
 unzip "${z3_version}.zip"
 sudo cp "${z3_version}/bin/z3" $install_dir
 
-# Install libzmq
-git clone https://github.com/zeromq/libzmq.git ./libzmq
-
-cd ./libzmq
-
-./autogen.sh
-./configure
-sudo make
-sudo make install
-
-cd $TRAVIS_BUILD_DIR
-
 # Retrieve opam.
-wget -qq https://raw.github.com/ocaml/opam/master/shell/opam_installer.sh -O - | sh -s /usr/local/bin 4.04.0
-export OPAMYES=1
+wget -qq https://raw.githubusercontent.com/ocaml/opam/master/shell/install.sh -O - | sh -s
 eval $(opam config env)
 
 # Install ocaml packages needed for Kind 2.
