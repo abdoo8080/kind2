d90512707a46516539a0a72083646b5f1372c63b
Abdalrhman M Mohamed <abdoo8080@outlook.com>
2020-06-12 00:27:58 -0500

    Another travis fix.
    Signed-off-by: Abdalrhman M Mohamed <abdoo8080@outlook.com>

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -4,7 +4,7 @@ if [ "$TRAVIS_OS_NAME" = "linux" ]; then
   install_dir="/usr/bin/z3"
   echo "deb http://download.opensuse.org/repositories/network:/messaging:/zeromq:/release-stable/Debian_9.0/ ./" >> /etc/apt/sources.list
   wget https://download.opensuse.org/repositories/network:/messaging:/zeromq:/release-stable/Debian_9.0/Release.key -O- | sudo apt-key add
-  apt-get install libzmq3-dev
+  sudo apt-get install libzmq3-dev
 elif [ "$TRAVIS_OS_NAME" = "osx" ]; then 
   z3_version="z3-4.7.1-x64-osx-10.11.6"
   install_dir="/usr/local/bin/z3"
@@ -18,7 +18,7 @@ sudo cp "${z3_version}/bin/z3" $install_dir
 # Retrieve opam.
 wget -qq https://raw.githubusercontent.com/ocaml/opam/master/shell/install.sh -O - | sh -s
 opam init
-eval $(opam config env)
+y
 
 # Install ocaml packages needed for Kind 2.
 opam install -t -v .
